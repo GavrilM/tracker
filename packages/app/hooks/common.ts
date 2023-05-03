2cf243864f5e225c5091ed8e00b7f0ea20974868
GavrilM <ghmoniaga@gmail.com>
2023-05-03 11:44:45 -0700

    question re asked bug

--- packages/app/hooks/common.ts
+++ packages/app/hooks/common.ts
@@ -4,8 +4,11 @@ import { CollectReview } from './types/QueryResult';
 import { MetricType } from '@my/ui';
 
 export function genQuestions(metrics: Array<Metric>, targetDate: Date): Array<Metric> {
-  const date = moment(targetDate)
+  const date = formatDate(targetDate)
   return metrics.filter(({ question_freq, last_point }) => {
+    const lastPointDate = formatDate(last_point?.timestamp)
+    if(date.toISOString() === lastPointDate.toISOString())
+      return false
     if(question_freq.month_date != undefined) {
       if(question_freq.month_date === 'last day'){
         const month = date.month()
@@ -18,9 +21,9 @@ export function genQuestions(metrics: Array<Metric>, targetDate: Date): Array<Me
       return question_freq.weekdays.includes(date.day())
     } else {
       const daysSinceLastPoint = last_point?.timestamp 
-        ? date.diff(moment(last_point.timestamp), 'day')
+        ? date.diff(lastPointDate, 'day')
         : Infinity 
-      return daysSinceLastPoint > question_freq.days
+      return daysSinceLastPoint >= question_freq.days
     }
   })
 }
@@ -54,3 +57,7 @@ export function genQuestionReview(metrics: Array<Metric>, pointLookup): CollectR
 }
 
 export const withOwnerId = (o, userId) => Object.assign({owner_id: {link: userId}}, o)
+
+function formatDate(date) {
+  return moment(moment(date).format('YYYYMMDD'))
+}
